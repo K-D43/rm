bd8fa933f6bf1069b81eea5d0b2369e0811017fd
Deependra Kumawat <kumawatdeependra43@gmail.com>
2023-01-10 22:49:58 +0530

    done

---
+++ app/src/main/java/com/example/retroapi/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.retroapi
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.retroapi.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getData()
+
+        binding.btnNext.setOnClickListener {
+            getData()
+        }
+
+    }
+
+    private fun getData() {
+
+        val progressDialog=ProgressDialog(this)
+        progressDialog.setMessage("Please wait while data is fetching")
+        progressDialog.show()
+
+        try{
+            RetrofitInstance.apiInterface.getData().enqueue(object : Callback<responseDataClass?> {
+
+
+
+                override fun onResponse(
+                    call: Call<responseDataClass?>,
+                    response: Response<responseDataClass?>,
+                ) {
+                    binding.title.text=response.body()?.title
+                    Log.d("tag","name" )
+                    binding.author.text=response.body()?.author
+                    Log.d("tag","author" )
+                    Glide.with(this@MainActivity).load(response.body()?.url).into(binding.image)
+                    Log.d("tag","image" )
+                    progressDialog.dismiss()
+                }
+
+                override fun onFailure(call: Call<responseDataClass?>, t: Throwable) {
+                    Toast.makeText(this@MainActivity,"Error",Toast.LENGTH_SHORT).show()
+                    progressDialog.dismiss()
+                }
+            })
+        }catch (e: Exception){
+
+        }
+    }
+}
